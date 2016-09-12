1661679fa149166759ad5b650d198d3d01f3eb76
ossama mikhail <usama.cpp@gmail.com>
2016-09-12 11:02:45 -0500

    try use the framework

--- FastCacheDemo/ViewController.swift
+++ FastCacheDemo/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
+import FastCache
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSURLProtocol.registerClass(URLFastCache.self)
     }
 
     override func didReceiveMemoryWarning() {
