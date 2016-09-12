46fea054fe48abbfe33ac33021cf89516a0b6e0e
ossama mikhail <usama.cpp@gmail.com>
2016-09-12 10:43:14 -0500

    demo target created

---
+++ FastCacheDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FastCacheDemo
+//
+//  Created by ossama mikhail on 9/12/16.
+//  Copyright © 2016 ossama mikhail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
