4af3e06036c1015469b6769209437c6c66bb2b96
ossama mikhail <usama.cpp@gmail.com>
2016-09-12 10:41:53 -0500

    basic classes aded

---
+++ FastCache/FastCache.swift
@@ -0,0 +1,13 @@
+//
+//  FastCache.swift
+//  FastCache
+//
+//  Created by ossama mikhail on 9/12/16.
+//  Copyright © 2016 ossama mikhail. All rights reserved.
+//
+
+import Foundation
+
+class FastCache {
+    
+}
---
+++ FastCache/URLFastCache.swift
@@ -0,0 +1,12 @@
+//
+//  NSFastCache.swift
+//  FastCache
+//
+//  Created by ossama mikhail on 9/12/16.
+//  Copyright © 2016 ossama mikhail. All rights reserved.
+//
+
+import Foundation
+
+class URLFastCache : NSURLProtocol {
+}
