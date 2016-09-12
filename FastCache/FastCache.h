35700f71a3ccbdacf85c3118425c786e06f8c0d3
ossama mikhail <usama.cpp@gmail.com>
2016-09-12 09:40:15 -0500

    Initial Commit

---
+++ FastCache/FastCache.h
@@ -0,0 +1,19 @@
+//
+//  FastCache.h
+//  FastCache
+//
+//  Created by ossama mikhail on 9/12/16.
+//  Copyright © 2016 ossama mikhail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FastCache.
+FOUNDATION_EXPORT double FastCacheVersionNumber;
+
+//! Project version string for FastCache.
+FOUNDATION_EXPORT const unsigned char FastCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastCache/PublicHeader.h>
+
+
